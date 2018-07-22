72662a11484d7414acc1dc76ce2d6a9722bbc8e1
Can PERK <bayms8416@gmail.com>
2018-07-22 13:58:19 +0300

    components' workout

--- BilgeAdam.TaskList/BilgeAdam.TaskList/wwwroot/js/site.js
+++ BilgeAdam.TaskList/BilgeAdam.TaskList/wwwroot/js/site.js
@@ -1,9 +1,38 @@
 ﻿var yms = {
-    components: {}
+    components: {},
+    models: {
+        task: {
+            id: 0,
+            title: ""
+        }
+    },
+    helpers: {
+        increment: function () {
+            return ++yms.index;
+        },
+        createTask: function (t) {
+            var newTask = yms.models.task;
+            newTask.id = yms.helpers.increment();
+            newTask.title = t;
+            return newTask;
+        }
+    },
+    index: 0
 };
 
 yms.components.todo = Vue.component("yms-todo", {
-    template: '#tmpTodo'
+    template: '#tmpTodo',
+    data: function () {
+        return {
+            tasks: []
+        };
+    },
+    methods: {
+        addNew: function (title) {
+            var task = yms.helpers.createTask(title);
+            //add to tasks
+        }
+    }
 });
 
 yms.components.inProgress = Vue.component("yms-inprogress", {
