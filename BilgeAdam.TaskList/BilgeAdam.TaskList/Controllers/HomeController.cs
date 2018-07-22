c2496fb0a199e2785593aa3c07ebd56fd1eae0ac
Can PERK <bayms8416@gmail.com>
2018-07-22 13:33:35 +0300

    injection of vue & components

--- BilgeAdam.TaskList/BilgeAdam.TaskList/Controllers/HomeController.cs
+++ BilgeAdam.TaskList/BilgeAdam.TaskList/Controllers/HomeController.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using BilgeAdam.TaskList.Models;
+﻿using Microsoft.AspNetCore.Mvc;
 
 namespace BilgeAdam.TaskList.Controllers
 {
--- BilgeAdam.TaskList/BilgeAdam.TaskList/wwwroot/js/site.js
+++ BilgeAdam.TaskList/BilgeAdam.TaskList/wwwroot/js/site.js
@@ -1,1 +1,19 @@
-﻿// Write your JavaScript code.
+﻿var yms = {
+    components: {}
+};
+
+yms.components.todo = Vue.component("yms-todo", {
+    template: '#tmpTodo'
+});
+
+yms.components.inProgress = Vue.component("yms-inprogress", {
+    template: '#tmpInprogress'
+});
+
+yms.components.done = Vue.component("yms-done", {
+    template: '#tmpDone'
+});
+
+new Vue({
+    el: "#taskList"
+});
