da8b5cd069c7fee9e4d21db7fb3fff1d936cb045
Can PERK <bayms8416@gmail.com>
2018-07-29 13:19:01 +0300

    inprogress works

--- BilgeAdam.TaskList/BilgeAdam.TaskList/wwwroot/js/site.js
+++ BilgeAdam.TaskList/BilgeAdam.TaskList/wwwroot/js/site.js
@@ -25,27 +25,54 @@ yms.components.todo = Vue.component("yms-todo", {
     data: function () {
         return {
             tasks: [],
-            title : ""
+            title: ""
         };
     },
     methods: {
         addNew: function () {
             var task = yms.helpers.createTask(this.title);
+            if (!task.title) {
+                return;
+            }
             this.tasks.push(task);
             this.title = "";
+        },
+        remove: function (index) {
+            this.tasks.splice(index, 1);
         },
         sendToInProgress: function (index, task) {
             this.$parent.saveToInProgress(task)
             this.tasks.splice(index, 1);
         }
+    },
+    mounted: function () {
+        var self = this;
+        self.$parent.$on("onSentToToDo", function (task) {
+            self.tasks.push(task);
+        });
     }
 });
 yms.components.inProgress = Vue.component("yms-inprogress", {
     template: '#tmpInprogress',
     data: function () {
         return {
-            tasks : []
+            tasks: []
         };
+    },
+    methods: {
+        sendToToDo: function (index, task) {
+            this.$parent.saveToToDo(task);
+            this.tasks.splice(index, 1);
+        },
+        sendToDone: function (index, task) {
+
+        }
+    },
+    mounted: function () {
+        var self = this;
+        self.$parent.$on("onSentToInProgress", function (task) {
+            self.tasks.push(task);
+        });
     }
 });
 
@@ -57,7 +84,10 @@ new Vue({
     el: "#taskList",
     methods: {
         saveToInProgress: function (task) {
-            console.log("Aldım");
+            this.$emit("onSentToInProgress", task);
+        },
+        saveToToDo: function (task) {
+            this.$emit("onSentToToDo", task);
         }
     }
 });
